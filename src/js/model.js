352a0855807ba17274d4b00dc7407334b360235e
KR1ISTIN <kristin.desalme@gmail.com>
2023-11-14 21:32:52 -0600

    added err handling for loadRecipe func

--- src/js/model.js
+++ src/js/model.js
@@ -7,28 +7,34 @@ export const state = {
 // loadRecipe is going to change our state object 
 // then the state will contain the recipe at which then the controller will take the recipe out of there 
 export const loadRecipe = async function(id) {
-        // res is going to return a promise 
-        const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`);
+    try{
+         // res is going to return a promise 
+         const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`);
     
-        // .json() is going to return another promise 
-        const data = await res.json();
-        console.log(res, data);
-    
-        if(!res.ok) throw new Error(`${data.message} (${res.status})`);
-    
-        // obj destructuring bc data.data has a proptery name recipe 
-        let {recipe} = data.data;
-        // creating a new obj with data
-        state.recipe = {
-          id: recipe.id,
-          title: recipe.title,
-          publisher: recipe.publisher,
-          sourceUrl: recipe.source_url,
-          image: recipe.image_url,
-          servings: recipe.servings,
-          cookingTime: recipe.cooking_time,
-          ingredients: recipe.ingredients
-        };
-        console.log(state.recipe);
+         // .json() is going to return another promise 
+         const data = await res.json();
+         console.log(res, data);
+     
+         if(!res.ok) throw new Error(`${data.message} (${res.status})`);
+     
+         // obj destructuring bc data.data has a proptery name recipe 
+         let {recipe} = data.data;
+         // creating a new obj with data
+         state.recipe = {
+           id: recipe.id,
+           title: recipe.title,
+           publisher: recipe.publisher,
+           sourceUrl: recipe.source_url,
+           image: recipe.image_url,
+           servings: recipe.servings,
+           cookingTime: recipe.cooking_time,
+           ingredients: recipe.ingredients
+         };
+         console.log(state.recipe);
+ 
 
+    } catch (err) {
+        console.log(err)
+    }
+       
 }
